4e689747638753be164d4be7c05f1c5fd1c135f6
mandeepsidhu9191 <mandeepsidhu9191@gmail.com>
2026-01-18 20:23:40 -0500

    These changes for db migration

--- app/Infrastructure/ApplicationDbContext.cs
+++ app/Infrastructure/ApplicationDbContext.cs
@@ -1,4 +1,19 @@
-public class ApplicationDbContext
+using System;
+using app.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
 {
-    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Hospital> Hospitals { get; set; }
+    public DbSet<PlaceDetails> PlaceDetails { get; set; }
+    public DbSet<PlacePrediction> PlacePrediction { get; set; } = null!;
+    public DbSet<PlaceResult> PlaceResults { get; set; } = null!;
+   public DbSet<UserLocation> UserLocation { get; set; } = null!;
+
+
 }
