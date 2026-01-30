e70f68b0c43e5759663a008c21cd5e186ddaa90b
JHP <uk_admin@jprogramme.com>
2026-01-30 18:12:56 -0500

    Service filter implementation v2

--- app/Models/HospitalService.cs
+++ app/Models/HospitalService.cs
@@ -1,10 +1,16 @@
-﻿namespace app.Models
+﻿/*
+ * 
+ * HospitalService
+ * 
+ * To store lists of available service name, (Eg : Emergency Dept, Internal Medicine, Pediatrics, Ophthalmology, etc...)
+ * 
+ * */
+namespace app.Models
 {
     public class HospitalService
     {
         public int Id { get; set; }
         public string ServiceName { get; set; } = "";
-
         public virtual List<HospitalServiceAssignment>? Assignments { get; set; }
     }
 }
--- app/Models/HospitalServiceAssignment.cs
+++ app/Models/HospitalServiceAssignment.cs
@@ -1,12 +1,16 @@
-﻿namespace app.Models
+﻿/*
+ * 
+ * HospitalServiceAssignment
+ * 
+ * Schema for storing which services are offered in a hospital. 
+ * 
+ * */
+namespace app.Models
 {
     public class HospitalServiceAssignment
     {
         public int Id { get; set; }
         public virtual HospitalService Service { get; set; } = null!;
-
         public virtual Hospital Hospital { get; set; } = null!;
-
-
     }
 }
